0b53a64f8fcbaf40fb1b817e71519d2d4be12958
Rebecca Clark <a.rebeccaclark126@gmail.com>
2023-12-11 09:50:04 +0000

    Final project

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Harry-Potter-App
+
+App Link:
+    https://a-rebecca.github.io/Harry-Potter-App/
+
+
+## Idea overview:
+- I wanted to create dictionary where users can look up potions as spells. The format is inspired from classic games, with potions and spell cards. The idea of this project is to be a first version, that more advanced features can be added to later.
+
+- Users begin by simply clicking on either the potions or spells images - that makes the results page visible. On the results page, potions and/ or spells appear inside cards showing an image and a short overview of the result:
+    - Potions: Title, image, effect
+    - Spells: Title, image, effect
+
+- When clicking on a particular search result, the user receive a details card that explain more about the potion
+or spell:
+    Potion: Title, image, effect, ingredients, difficulty.
+    Spell: Incantation, effect, hand motion.
+
+- Users can go back and forth between the potions and spells simply by clicking on the respective top images or titles.
+    The image/ title of the results that are currently not being used is visually out of focus, however still shows that it's
+    clickable when hovered.
+
+
+
+## Technical hurdle: 
+- As someone new to Vanilla JS, I struggled in general with looping/ using the data retrieved from the API. One example of this was understanding how to solve none-iterable objects.
+
+
+## Something I learned:
+- I got a much better understanding of the flow of the code, and how to combine Vanilla JS with HTML and CSS. 
+    As well, I learned how to apply filters to API end-points and got a better understanding of how to do axios calls.
+
+
+## Where to next:
+- In the future, I'd like to add pagination so that the amount of search results can be shown on different pages.
+    As well I'd like to add filters based on e.g. level of difficulty, and the possibility to jump to a certain letter (as in
+    a real dictionary).
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 //URL for Axios calls
-const potionsUrl = `https://api.potterdb.com/v1/potions?page[size]=10`;
-const spellsUrl = `https://api.potterdb.com/v1/spells?filter[incantation_not_eq]=null&page[size]=10`;
-const potionsPage2 = `https://api.potterdb.com/v1/potions?page[number]=2`;
+const potionsUrl = `https://api.potterdb.com/v1/potions?filter[characteristics_not_eq]=null&filter[effect_not_eq]=null&?filter[ingredients_not_eq]=null&filter[difficulty_not_eq]=nullpage[size]=40`;
+const spellsUrl = `https://api.potterdb.com/v1/spells?filter[incantation_not_eq]=null&filter[hand_not_eq]=null&filter[effect_not_eq]=null&page[size]=40`;
 
 // DOM Access
 const potionsDiv = document.querySelector("#potions");
@@ -10,20 +9,19 @@ const spellsDiv = document.querySelector("#spells");
 const spellsResultsDiv = document.querySelector("#spellsResults");
 const potionsDetailsDiv = document.querySelector("#potionsDetails");
 const spellsDetailsDiv = document.querySelector("#spellsDetails");
-const hogImg = document.querySelector("#bannerImage");
+const castle = document.querySelector("#castle");
 
-//Functions for axios calls
+////Functions for axios calls////
 
 //Potions call
-const potionCalls = function (){
+const potionCalls = function () {
   axios
-  .get(potionsUrl)
+    .get(potionsUrl)
 
-  .then(function (response) {
-    console.log(response.data.data);
+    .then(function (response) {
 
-    for (const potions of response.data.data) {
-      potionsResultsDiv.innerHTML += `
+      for (const potions of response.data.data) {
+        potionsResultsDiv.innerHTML += `
         <div id="potionsWrapper" data-id="${potions.id}">
         <h3>${potions.attributes.name}</h3>
         <img src=
@@ -34,115 +32,105 @@ const potionCalls = function (){
         }
         ${potions.attributes.image}>
         <p>Effect: 
-        ${
-          potions.attributes.effect === null
-            ? "Unknown"
-            : potions.attributes.effect
-        }
         ${potions.attributes.effect} </p>
         </div>
-    `;
-    }
-  })
-  .catch(function (err) {
-    console.log(`Error loading data`, err);
-  });
-}
-
-//Spells call
-const spellsCalls = function(){
-  axios
-    .get(spellsUrl)
-
-    .then(function (response) {
-      for (const spells of response.data.data) {
-        spellsResultsDiv.innerHTML += `
-    <h3>${spells.attributes.name}</h3>
-    <img src=${spells.attributes.image}>
-    <p>Effect: ${spells.attributes.effect}</p>
     `;
       }
     })
     .catch(function (err) {
       console.log(`Error loading data`, err);
     });
-}
+};
+
+//Spells call
+const spellsCalls = function () {
+  axios
+    .get(spellsUrl)
+
+    .then(function (response) {
+
+      for (const spells of response.data.data) {
+        spellsResultsDiv.innerHTML += `
+    <div id="spellsWrapper" data-id="${spells.id}">       
+    <h3>${spells.attributes.name}</h3>
+    <img src=
+    ${
+      spells.attributes.image === null
+        ? "https://cdn.pixabay.com/photo/2013/07/12/14/14/wand-148059_1280.png"
+        : spells.attributes.image
+    }>
+    <p>Effect: ${spells.attributes.effect}</p>
+    </div>
+    `;
+      }
+    })
+    .catch(function (err) {
+      console.log(`Error loading data`, err);
+    });
+};
+
+////Styling functions////
+
+//Click on potions and get potions results
+const potionsResults = function () {
+  potionsDiv.addEventListener("click", function () {
+    potionsResultsDiv.style.display = "flex";
+    spellsResultsDiv.style.display = "none";
+    potionsDiv.style.opacity = "100";
+    spellsDiv.style.opacity = "0.2";
+    spellsDetailsDiv.style.display = "none";
+    potionsDetailsDiv.style.display = "none";
+    castle.style.display = "none";
+  });
+};
+
+//Click on spells and get spell results
+const spellsResults = function () {
+  spellsDiv.addEventListener("click", function () {
+    spellsDiv.style.opacity = "100";
+    spellsResultsDiv.style.display = "flex";
+    potionsResultsDiv.style.display = "none";
+    potionsDiv.style.opacity = "0.2";
+    potionsDetailsDiv.style.display = "none";
+    spellsDetailsDiv.style.display = "none";
+    castle.style.display = "none";
+  });
+};
 
 // Initial API call upon script load
 potionCalls();
 spellsCalls();
 
-//Click on potions and get potions results
-potionsDiv.addEventListener("click", function () {
-  potionsResultsDiv.style.display = "flex";
-  spellsResultsDiv.style.display = "none";
-  potionsDiv.style.opacity = "100";
-  spellsDiv.style.opacity = "0.2";
-  spellsResultsDiv.style.display = "none";
-  potionsDetailsDiv.style.display = "none";
-  hogImg.style.display = "none";
-
-  
-});
-
-// Pagination
-/*
-potionsDiv.addEventListener("click", function () {
-  axios
-    .get(potionsPage2)
-
-    .then(function (response) {
-      console.log(response.data.data);
-
-      for (const potionsPage of response.data.data) {
-        potionsResultsDiv.innerHTML += `
-        <ul>
-        <li><a href="${potionsPage}>1</a></li>
-        <li>2</li>
-        <li>3</li>
-        </ul>
-        `;
-      }
-    })
-    .catch(function (err) {
-      console.log(`Error loading data`, err);
-    });
-});*/
+//Styling functions
+potionsResults();
+spellsResults();
 
 // Click on potion results and get potion details
 potionsResultsDiv.addEventListener("click", function (ev) {
-  spellsDiv.style.opacity = "0.2";
-  spellsResultsDiv.style.display = "none";
-  potionsDiv.style.opacity = "100";
-  hogImg.style.display = "none";
-
   if (ev.target.closest("div#potionsWrapper")) {
     potionsResultsDiv.style.display = "none";
     potionsDetailsDiv.style.display = "flex";
-    console.log(ev.target.closest("div#potionsWrapper"));
 
     const potionsId = ev.target.closest("div#potionsWrapper").dataset.id;
     const potionsIdUrl = `https://api.potterdb.com/v1/potions/${potionsId}`;
 
-    console.log(potionsIdUrl);
     axios
       .get(potionsIdUrl)
 
       .then(function (response) {
-        console.log(response.data);
 
         const map = new Map();
         map.set(response.data.data);
-
+        console.log(map.keys());
         for (const potionDetails of map.keys()) {
           const characteristics = map.get(potionDetails);
-          console.log(characteristics, potionDetails);
           potionsDetailsDiv.innerHTML = `
       <div id=“potionsCard” data-id=“${potionDetails.id}“>
       <h3>${potionDetails.attributes.name}</h3>
       <img src=${potionDetails.attributes.image}>
-      <p>Effect: ${potionDetails.attributes.effect}</p>
-      <p>Ingredients: ${potionDetails.attributes.effect}</p>
+      <p>Effect: ${potionDetails.attributes.effect}</p><br>
+      <p>Ingredients: ${potionDetails.attributes.ingredients}</p><br>
+      <p>Difficulty: ${potionDetails.attributes.difficulty}</p>
       </div>
       `;
         }
@@ -153,14 +141,41 @@ potionsResultsDiv.addEventListener("click", function (ev) {
   }
 });
 
-//Click on spells and get spell results
-spellsDiv.addEventListener("click", function () {
-  spellsDiv.style.opacity = "100";
-  spellsResultsDiv.style.display = "flex";
-  potionsResultsDiv.style.display = "none";
+// Click on spell results and get spell details
+spellsResultsDiv.addEventListener("click", function (ev) {
   potionsDiv.style.opacity = "0.2";
-  potionsResultsDiv.style.display = "none";
-  potionsDetailsDiv.style.display = "none";
-  hogImg.style.display = "none";
+  spellsDiv.style.opacity = "100";
 
+  if (ev.target.closest("div#spellsWrapper")) {
+    potionsResultsDiv.style.display = "none";
+    potionsDetailsDiv.style.display = "none";
+    spellsDetailsDiv.style.display = "flex";
+    spellsResultsDiv.style.display = "none";
+    castle.style.display = "none";
+
+    const spellsId = ev.target.closest("div#spellsWrapper").dataset.id;
+    const spellsIdUrl = `https://api.potterdb.com/v1/spells/${spellsId}`;
+
+    axios
+      .get(spellsIdUrl)
+
+      .then(function (response) {
+        console.log(response.data);
+
+        const spellDetails = response.data.data
+          console.log(spellDetails);
+          spellsDetailsDiv.innerHTML = `
+            <div id=“spellCard” data-id=“${spellDetails.id}“>
+              <h3>${spellDetails.attributes.name}</h3>
+              <img src=${spellDetails.attributes.image}><br>
+              <p>Incantation: ${spellDetails.attributes.incantation}</p><br>
+              <p>Effect: ${spellDetails.attributes.effect}</p><br>
+              <p>Hand motion: ${spellDetails.attributes.hand}</p>
+            </div>
+          `;
+      })
+      .catch(function (err) {
+        console.log(`Error loading data`, err);
+      });
+  }
 });
